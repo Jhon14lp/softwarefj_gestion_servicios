cdd3e7bbacdc486fb2a0d12cc97fc6d33d8a7f58
Jhon14lp <jhoncortes90@gmail.com>
2026-05-07 13:21:47 -0500

    mod nam txt'delite'bsiatxt

--- interfaz.py
+++ interfaz.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox, ttk
-# Importamos desde tus otros archivos
+#modls
 from sistema import SistemaGestion
 from cliente import Cliente
 from reserva import ReservaSala
